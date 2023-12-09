39395a641ebb198844b253183cc54deff30f9c5d
Kumonryuu <97722395+Kumonryuu@users.noreply.github.com>
2023-12-09 17:06:44 -0500

    - Drafting shop.html, added banner

--- js/script.js
+++ js/script.js
@@ -8,8 +8,7 @@ function $(selector) {
 // Create a DOMcontentLoaded event listener for contact-us.html
 function contact_script() {
     // Create form validation for the form in contact-us.html
-    document.addEventListener("DOMContentLoaded", e => {
-        const form = $("#contact-form");
+    document.addEventListener("DOMContentLoaded", () => {
         const name = $("#name").value;
         const email = $("#email").value;
         const message = $("#message").value;
@@ -40,5 +39,79 @@ function contact_script() {
             });
         });
     });
-}
 
+    // utility functions
+    if (!Util) function Util() {
+    }
+
+
+    Util.hasClass = function (el, className) {
+        return el.classList.contains(className);
+    };
+
+    Util.addClass = function (el, className) {
+        var classList = className.split(' ');
+        el.classList.add(classList[0]);
+        if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+    };
+
+    Util.removeClass = function (el, className) {
+        var classList = className.split(' ');
+        el.classList.remove(classList[0]);
+        if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+    };
+
+    Util.setAttributes = function (el, attrs) {
+        for (var key in attrs) {
+            el.setAttribute(key, attrs[key]);
+        }
+    };
+
+    Util.moveFocus = function (element) {
+        if (!element) element = document.getElementsByTagName('body')[0];
+        element.focus();
+        if (document.activeElement !== element) {
+            element.setAttribute('tabindex', '-1');
+            element.focus();
+        }
+    };
+
+    Util.getIndexInArray = function (array, el) {
+        return Array.prototype.indexOf.call(array, el);
+    };
+
+    Util.cssSupports = function (property, value) {
+        return CSS.supports(property, value);
+    };
+
+    Util.extend = function () {
+        var extended = {};
+        var deep = false;
+        var i = 0;
+        var length = arguments.length;
+
+        if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]') {
+            deep = arguments[0];
+            i++;
+        }
+
+        var merge = function (obj) {
+            for (var prop in obj) {
+                if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+                    if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
+                        extended[prop] = extend(true, extended[prop], obj[prop]);
+                    } else {
+                        extended[prop] = obj[prop];
+                    }
+                }
+            }
+        };
+
+        for (; i < length; i++) {
+            var obj = arguments[i];
+            merge(obj);
+        }
+
+        return extended;
+    };
+}
