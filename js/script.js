2b4546f3a36cd0af12ef001e6d7c09c6567dc993
Kumonryuu <97722395+Kumonryuu@users.noreply.github.com>
2023-12-08 23:47:31 -0500

    - Drafted the shop.html and did the basic css

--- js/script.js
+++ js/script.js
@@ -1,69 +1,44 @@
 "use strict";
 
+// Create a query selector
 function $(selector) {
     return document.querySelector(selector);
 }
 
+// Create a DOMcontentLoaded event listener for contact-us.html
 function contact_script() {
+    // Create form validation for the form in contact-us.html
     document.addEventListener("DOMContentLoaded", e => {
         const form = $("#contact-form");
-        const name = $("#name");
-        const email = $("#email");
-        const message = $("#message");
+        const name = $("#name").value;
+        const email = $("#email").value;
+        const message = $("#message").value;
+        const submit = $("#submitBtn");
 
-        // Add input event listeners to the input fields
-        name.addEventListener("input", validateName);
-        email.addEventListener("input", validateEmail);
-        message.addEventListener("input", validateMessage);
+        let errorMessage = "";
 
-        form.addEventListener("submit", (ev) => {
-            ev.preventDefault();
+        submit.addEventListener("click", () => {
+            if (name.validity.valueMissing) {
+                errorMessage += "Name is required.\n";
+            }
 
-            // Reset error messages and classes
-            ["name", "email", "message"].forEach(id => {
-                $(`#${id}`).classList.remove("error");
-                $(`#${id}-error`).textContent = "";
+            if (!email.validity.valid) {
+                errorMessage += "Email is required.\n";
+            }
+
+            if (message.validity.valueMissing) {
+                errorMessage += "Message is required.\n";
+            }
+
+            submit.addEventListener("click", ev => {
+                if (errorMessage.length > 0) {
+                    alert(errorMessage);
+                    ev.preventDefault();
+                } else {
+                    alert("Message sent successfully!");
+                }
             });
-
-            // Validate the input fields when the form is submitted
-            validateName();
-            validateEmail();
-            validateMessage();
-
-            if (!form.querySelector(".error")) {
-                alert("Message sent successfully!");
-                form.reset();
-            }
         });
-
-        function validateName() {
-            if (name.value.trim() === "") {
-                name.classList.add("error");
-                $("#name-error").textContent = "Name is required.";
-            } else {
-                name.classList.remove("error");
-                $("#name-error").textContent = "";
-            }
-        }
-
-        function validateEmail() {
-            if (email.value.trim() === "") {
-                email.classList.add("error");
-                $("#email-error").textContent = "Email is required.";
-            } else {
-                email.classList.remove("error");
-                $("#email-error").textContent = "";
-            }
-        }
-
-        function validateMessage() {
-            if (message.value.trim() === "") {
-                message.classList.add("error");
-                $("#message-error").textContent = "Message is required.";
-            } else {
-                message.classList.remove("error");
-                $("#message-error").textContent = "";
-            }
-        }
     });
 }
+
