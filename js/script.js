d1c8a5289598fda450dc5774ff4a9c5cc5aa5609
Kumonryuu <97722395+Kumonryuu@users.noreply.github.com>
2023-12-11 18:39:46 -0500

    - Drafting jquery for shop.html

--- js/script.js
+++ js/script.js
@@ -72,3 +72,12 @@ function contact_script() {
         });
     });
 }
+
+function shop_script() {
+    document.addEventListener("DOMContentLoaded", () => {
+        $('.open-cart').magnificPopup({
+            type:'inline',
+            midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        });
+    });
+}
